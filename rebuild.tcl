282f60192c02b7e05fe1cbe8bcda8bc71d84da88
nachtspyder04 <sskarole.1504@gmail.com>
2026-06-18 19:12:25 +0530

    Update tcl and .gitignore

--- rebuild.tcl
+++ rebuild.tcl
@@ -1,7 +1,6 @@
-# Create project
 create_project mat_mul_test ./mat_mul_test -part xc7a100tfgg484-2L
 
-# RTL sources
+# RTL
 add_files ./rtl/mat_mul.v
 add_files ./rtl/row_col.v
 add_files ./rtl/mat_mul_wrapper.v
@@ -10,22 +9,10 @@ add_files ./rtl/mat_mul_wrapper.v
 add_files -fileset constrs_1 ./constraints/normal_constraints.xdc
 add_files -fileset constrs_1 ./constraints/late_constraints.xdc
 
-# Create block design
-source ./bd/design_1.tcl
-
-# Generate BD outputs
-generate_target all [get_files design_1.bd]
-
-# Create HDL wrapper
-make_wrapper -files [get_files design_1.bd] -top
-
-# Add generated wrapper
-add_files -norecurse \
-[get_files ./mat_mul_test/mat_mul_test.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v]
-
-# Set top
-set_property top design_1_wrapper [current_fileset]
-
 update_compile_order -fileset sources_1
 
-save_project_as mat_mul_pcie
+# Build BD
+source ./bd/design_1.tcl
+
+# Save project
+save_project_as mat_mul_test
